67fbc3dfd4e8eb8bb79844336139b2a035b689eb
Indrajit <indrajit30623@gmail.com>
2023-09-21 23:35:43 +0530

    updated nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -14,8 +14,8 @@ const Nav = () => {
       <a class="navbar-brand" href="#">AI Gift Shop</a>
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="/">Home</a>
-          {/* <NavLink to="/" class="nav-link active" aria-current="page" style={{ color: 'white', textDecoration: 'none' }}>Home</NavLink> */}
+          {/* <a class="nav-link active" aria-current="page" href="/">Home</a> */}
+          <NavLink to="/" className="nav-link active" aria-current="page" style={{ color: 'white', textDecoration: 'none' }}>Home</NavLink>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="#">About</a>
